605121bb5c192362efd11e2590f77f0a8419225b
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-06-24 14:22:12 +0300

    forward/backward search

--- pretext.c
+++ pretext.c
@@ -384,15 +384,34 @@ void editor_save()
 
 void editor_find_callback(char* query, int key)
 {
-    if (key == '\r' || key == '\x1b') return;
+    static int last_match = -1;
+    static int direction = 1;
 
-    for(int i = 0; i < E.numrows; ++i)
-    {
-        erow* row = &E.row[i];
-        char* match = strstr(row->render, query);
-        if(match)
-        {
-            E.cy = i;
+    if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
+        return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
+    }
+
+    if (last_match == -1) direction = 1;
+
+    int current = last_match;
+    for (int i = 0; i < E.numrows; i++) {
+        current += direction;
+        if (current == -1) current = E.numrows - 1;
+        else if (current == E.numrows) current = 0;
+        erow *row = &E.row[current];
+        char *match = strstr(row->render, query);
+        if (match) {
+            last_match = current;
+            E.cy = current;
             E.cx = row_rx_to_cx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
@@ -407,7 +426,7 @@ void editor_find()
     int saved_coloff = E.coloff;
     int saved_rowoff = E.rowoff;
 
-    char *query = prompt("Search: %s (ESC to cancel)", editor_find_callback);
+    char *query = prompt("Search: %s (Use ESC/Arrows/Enter)", editor_find_callback);
     if(query){
         free(query);
     } else {
